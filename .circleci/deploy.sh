d63ca71f572fee0665473f0e509c49230a2e5a8c
blablanumerodeux <blablanumerodeux@gmail.com>
2019-06-22 22:29:28 -0400

    features/75_front_deploy: feat: volumes !

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,6 +7,5 @@ docker-compose -f docker-compose-prod.yml pull
 docker-compose -f docker-compose-prod.yml down
 docker-compose -f docker-compose-prod.yml up -d couch
 sleep 50
-./configure-node.sh 
 docker-compose -f docker-compose-prod.yml up -d app
 docker-compose -f docker-compose-prod.yml up -d app-ng
